fef57228c80d787de61f63cd3a8ea29f5defc274
phagun jain <500076430@stu.upes.ac.in>
2019-11-23 12:25:19 +0530

    ui changes1

--- app/src/main/java/com/example/smartirrigation/MainActivity.java
+++ app/src/main/java/com/example/smartirrigation/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.smartirrigation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText croptype,soiltype,currentwater;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        croptype=findViewById(R.id.crop);
+        soiltype=findViewById(R.id.soil);
+        currentwater=findViewById(R.id.current_water);
+
+    }
+
+    public void nextClicked(View view) {
+        String cropValue=croptype.getText().toString();
+        String soilValue=soiltype.getText().toString();
+        String currentvalue=
+
     }
 }
