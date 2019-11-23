05f0444dc25f7556bf3abfb96f9fdce3f33732ab
phagun jain <500076430@stu.upes.ac.in>
2019-11-23 14:13:29 +0530

    ui changes2

--- app/src/main/java/com/example/smartirrigation/MainActivity.java
+++ app/src/main/java/com/example/smartirrigation/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.smartirrigation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     EditText croptype,soiltype,currentwater;
-
+    TextView tapState,timeLeft;
 
 
     HashMap<String,Integer> cropsDatabase=new HashMap<>();
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         cropsDatabase.put("rice",1250);
+
         cropsDatabase.put("Groundnut",550);
         cropsDatabase.put("Sorghum",350);
         cropsDatabase.put("Maize",500);
@@ -29,15 +33,29 @@ public class MainActivity extends AppCompatActivity {
         croptype=findViewById(R.id.crop);
         soiltype=findViewById(R.id.soil);
         currentwater=findViewById(R.id.current_water);
-
+        tapState=findViewById(R.id.tap_state);
+        timeLeft=findViewById(R.id.time_left);
     }
-
+    long time;
     public void nextClicked(View view) {
         String cropValue=croptype.getText().toString().toLowerCase();
         String soilValue=soiltype.getText().toString().toLowerCase();
-        String currentvalue=currentwater.getText().toString().toLowerCase();
+        int currentvalue= Integer.parseInt(currentwater.getText().toString());
         int required=cropsDatabase.get(cropValue);
+        time=(required-currentvalue)/10;
+        tapState.setText("Tap open");
+        new CountDownTimer(time*1000, 1000) {
 
+            public void onTick(long millisUntilFinished) {
+                timeLeft.setText(millisUntilFinished/1000+"");
+            }
+
+            public void onFinish() {
+                tapState.setText("Tap closed");
+                timeLeft.setText("0");
+                Toast.makeText(MainActivity.this, "Tap Closed", Toast.LENGTH_SHORT).show();
+            }
+        }.start();
 
     }
 }
