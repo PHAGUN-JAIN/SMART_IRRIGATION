b90c3a2682de315e1fc7e93a3b4136121e0370c5
phagun jain <500076430@stu.upes.ac.in>
2019-11-23 12:49:41 +0530

    ui changes1

--- app/src/main/java/com/example/smartirrigation/MainActivity.java
+++ app/src/main/java/com/example/smartirrigation/MainActivity.java
@@ -8,10 +8,19 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     EditText croptype,soiltype,currentwater;
+    HashMap<String,Integer> cropsDatabase=new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cropsDatabase.put("rice",1250);
+        cropsDatabase.put("rice",1250);
+        cropsDatabase.put("rice",1250);
+        cropsDatabase.put("rice",1250);
+        cropsDatabase.put("rice",1250);
+        cropsDatabase.put("rice",1250);
+        cropsDatabase.put("rice",1250);
+        cropsDatabase.put("rice",1250);
         croptype=findViewById(R.id.crop);
         soiltype=findViewById(R.id.soil);
         currentwater=findViewById(R.id.current_water);
@@ -19,9 +28,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void nextClicked(View view) {
-        String cropValue=croptype.getText().toString();
-        String soilValue=soiltype.getText().toString();
-        String currentvalue=
+        String cropValue=croptype.getText().toString().toLowerCase();
+        String soilValue=soiltype.getText().toString().toLowerCase();
+        String currentvalue=currentwater.getText().toString().toLowerCase();
+        int required=cropsDatabase.get(cropValue);
+
 
     }
 }
